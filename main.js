cc43b8ae0750d645f30b709dcca166ec59431280
Andrea Sorensen <dreasor2987@icloud.com>
2023-04-20 17:53:18 -0700

    feature/add fighter buttons, createPlayer func

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+var classicGameChoices = ['stone', 'water', 'bird'];
+var difficultGameChoices = ['earth', 'air', 'fire', 'water'];
+var player;
+
 var classicGameBtn = document.querySelector('#classic-game-btn');
 var difficultGameBtn = document.querySelector('#difficult-game-btn');
 
+classicGameBtn.addEventListener(function(event){
+  changeViewClassic();
+
+});
+
+
+function createPlayer(name, token, wins=0){
+  player = {
+    name: name, 
+    token: token,
+    wins
+  }
+} return player
+
+function createGame(){
+  
+}
