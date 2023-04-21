4619cdbc81eccd49e4f1e5eb15630a4066551b17
Andrea Sorensen <dreasor2987@icloud.com>
2023-04-21 08:53:56 -0700

    feature/complete all structure

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var classicGameChoices = ['stone', 'water', 'bird'];
 var difficultGameChoices = ['earth', 'air', 'fire', 'water'];
-var player;
+
+// data model variables:
+var players = [];
+var userPlayer;
+var computerPlayer;
+var newGame;
+
 
 var classicGameBtn = document.querySelector('#classic-game-btn');
 var difficultGameBtn = document.querySelector('#difficult-game-btn');
 
 classicGameBtn.addEventListener(function(event){
-  changeViewClassic();
+
+    // createPlayer()
 
 });
 
@@ -19,6 +26,13 @@ function createPlayer(name, token, wins=0){
   }
 } return player
 
-function createGame(){
-  
+function createGame(userPlayer, computerPlayer, currentGameType){
+  newGame = {
+    players: [userPlayer, computerPlayer],
+    score: [userPlayer.wins, computerPlayer.wins],
+    // is the above line ?? necessary ??
+    gameType: currentGameType
+  }
+  return game
 }
+
